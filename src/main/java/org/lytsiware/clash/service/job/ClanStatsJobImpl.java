a0e2ef48df15a5f3d0857b51ad8af4dbc3b2940c
CLS <Christos.Lytsikas@arhs-dev-hellas.com>
2017-09-18 19:07:02 +0300

    adding flyway scipts for scheduler, updated log info

--- src/main/java/org/lytsiware/clash/service/job/ClanStatsJobImpl.java
+++ src/main/java/org/lytsiware/clash/service/job/ClanStatsJobImpl.java
@@ -20,61 +20,62 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClanStatsJobImpl implements ClanStatsJob {
 
-    private Logger logger = LoggerFactory.getLogger(ClanStatsJobImpl.class);
+	private Logger logger = LoggerFactory.getLogger(ClanStatsJobImpl.class);
 
-    @Autowired
-    IClanStatsService clanStatsService;
-    
-    @Autowired
-    WeekJobRepository weeklyJobRepository;
+	@Autowired
+	IClanStatsService clanStatsService;
 
-    @Override
-	@Scheduled(cron ="0 23 11 ? * MON ")
-    @Retryable(maxAttempts = 3, backoff = @Backoff(600000))
-    public void run() {
-    	try {
-	        logger.info("Job Triggered at {}", LocalDate.now());
-	        
-	        clanStatsService.updateDatabaseWithLatest();
-	        
-	        weeklyJobRepository.save(new WeeklyJob(new Week().minusWeeks(1).getWeek()));
-    	}catch (Exception e) {
+	@Autowired
+	WeekJobRepository weeklyJobRepository;
+
+	@Override
+	@Scheduled(cron = "0 23 11 ? * MON ")
+	@Retryable(maxAttempts = 3, backoff = @Backoff(600000))
+	public void run() {
+		try {
+			logger.info("Job Triggered at {}", LocalDate.now());
+
+			clanStatsService.updateDatabaseWithLatest();
+
+			weeklyJobRepository.save(new WeeklyJob(new Week().minusWeeks(1).getWeek()));
+		} catch (Exception e) {
 			logger.error("oops", e);
 			throw e;
 		}
-    }
-    
-   
+	}
 
 	@Override
 	public boolean shouldRun() {
-    	
-    	boolean result = false;
-    	
-    	Week previousWeek = new Week(LocalDate.now()).minusWeeks(1);
-    	
-    	//FIX the "dayTheSchedulerShouldHaveRun" should not be hardcoded but calculated from the cron expression:
-    	
-    	//There is a gap  between the day the timer runs and when the week changes (Sunday). In that case we dont want 
-    	//to update the db. 
-    	LocalDateTime dayTheSchedulerShouldHaveRun = previousWeek.getEndDate().atTime(LocalTime.of(12, 0)).plusDays(1); //monday at 13:00 o clock server time
-    	
-    	if (LocalDateTime.now().isAfter(dayTheSchedulerShouldHaveRun)) {
-    	
-    		logger.info("The scheduler should have already run.  Checking status");
-    		
-    		WeeklyJob latestRun = weeklyJobRepository.loadLatest();
-    		
-    		if (latestRun.getLatestWeek() == previousWeek.getWeek()){
-    			logger.info("Status OK");
-    		}else {
-    			logger.info("Scheduler has not ran: Starting job now");
-    			result = true;
-    		}
-    	}
-    	
-    	return result;
+		logger.info("Checking if scheduler should run");
+		boolean result = false;
+
+		Week previousWeek = new Week(LocalDate.now()).minusWeeks(1);
+
+		// FIX the "dayTheSchedulerShouldHaveRun" should not be hardcoded but
+		// calculated from the cron expression:
+
+		// There is a gap between the day the timer runs and when the week
+		// changes (Sunday). In that case we dont want
+		// to update the db.
+		// Monday 12:10 GMT+2 time 
+		LocalDateTime dayTheSchedulerShouldHaveRun = previousWeek.getEndDate().atTime(LocalTime.of(9, 10)).plusDays(1); 
+
+		if (LocalDateTime.now().isAfter(dayTheSchedulerShouldHaveRun)) {
+
+			WeeklyJob latestRun = weeklyJobRepository.loadLatest();
+
+			if (latestRun.getLatestWeek() == previousWeek.getWeek()) {
+				logger.info("scheduler has already been fired");
+			} else {
+				logger.info("Scheduler was not fired");
+				result = true;
+			}
+		} else {
+			logger.info("date is in between the gap");
+		}
+		
+		logger.info("Scheduler should run now: {}", result);
+		return result;
 	}
-    
 
 }
