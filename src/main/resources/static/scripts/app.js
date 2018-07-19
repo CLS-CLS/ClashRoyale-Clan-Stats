fd5ed138d1a61399dffdcfc6b3049c9e1149832a
Christos <chrislytsikas@gmail.com>
2018-07-19 19:47:06 +0300

    login #1

--- src/main/resources/static/scripts/app.js
+++ src/main/resources/static/scripts/app.js
@@ -340,6 +340,48 @@ app.controller("warStatsInputController", function($scope, $http){
     loadData();
 })
 
+
+app.controller("loginController", function($rootScope, $scope, $http, $location){
+
+    var authenticate = function(credentials, callback) {
+
+        var headers = credentials ? {authorization : "Basic "
+            + btoa(credentials.username + ":" + credentials.password)
+        } : {};
+
+        $http.get('user', {headers : headers}).success(function(data) {
+            if (data.name) {
+                $rootScope.authenticated = true;
+            } else {
+                $rootScope.authenticated = false;
+            }
+            callback && callback();
+        }).error(function() {
+            $rootScope.authenticated = false;
+            callback && callback();
+        });
+
+    }
+
+    authenticate();
+    $scope.credentials = {};
+
+    $scope.login = function() {
+        authenticate($scope.credentials, function() {
+        if ($rootScope.authenticated) {
+            $location.path("/");
+            $scope.error = false;
+        } else {
+            $location.path("/login");
+            $scope.error = true;
+        }
+    });
+
+
+
+})
+
+
 app.controller("playerWarStatsController", function($scope, $http, $routeParams, $timeout, colorfy, history) {
 
 	$scope.player;
