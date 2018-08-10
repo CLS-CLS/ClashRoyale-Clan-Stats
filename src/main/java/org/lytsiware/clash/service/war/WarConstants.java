3154144388524974285d9d387ce88345d5bd27e1
Christos <chrislytsikas@gmail.com>
2018-08-10 20:41:12 +0300

    war stats are calculated for 14 wars (was 7)

--- src/main/java/org/lytsiware/clash/service/war/WarConstants.java
+++ src/main/java/org/lytsiware/clash/service/war/WarConstants.java
@@ -3,7 +3,7 @@ package org.lytsiware.clash.service.war;
 public final class WarConstants {
 
     public static final int leagueDays = 2;
-    public static final int leagueSpan = 7;
+    public static final int leagueSpan = 14;
 
     private WarConstants(){}
 }
