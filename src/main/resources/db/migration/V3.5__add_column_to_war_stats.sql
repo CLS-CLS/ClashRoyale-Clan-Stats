a74071f40ea2321397d3bff25b9d5f9a6644df9d
Christos <chrislytsikas@gmail.com>
2019-05-27 00:45:56 +0300

    fixed broken script

--- src/main/resources/db/migration/V3.5__add_column_to_war_stats.sql
+++ src/main/resources/db/migration/V3.5__add_column_to_war_stats.sql
@@ -1,3 +1,2 @@
-ALTER TABLE player_war_stat ADD collection_games_player;
+ALTER TABLE player_war_stat ADD collection_games_played int;
 ALTER TABLE player_aggregation_war_stats ADD collection_games_missed int default 0;
-
