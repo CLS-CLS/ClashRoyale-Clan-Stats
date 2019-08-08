bf96a5ccb94870a57e027c157f2c8ecff52325e1
Christos <chrislytsikas@gmail.com>
2019-08-08 23:02:39 +0300

    1.runner to update stats 2.added forgotten script for new column

---
+++ src/main/java/org/lytsiware/clash/runners/UpdateWarScoreRunner.java
@@ -0,0 +1,32 @@
+package org.lytsiware.clash.runners;
+
+import lombok.extern.slf4j.Slf4j;
+import org.lytsiware.clash.domain.war.aggregation.PlayerAggregationWarStats;
+import org.lytsiware.clash.domain.war.aggregation.PlayerAggregationWarStatsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Component
+@Slf4j
+public class UpdateWarScoreRunner implements CommandLineRunner {
+
+    @Autowired
+    PlayerAggregationWarStatsRepository playerAggregationWarStatsRepository;
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+
+        List<PlayerAggregationWarStats> aggretationWarStats = playerAggregationWarStatsRepository.findAll();
+        for (PlayerAggregationWarStats warStat : aggretationWarStats) {
+            warStat.setScore((int) ((0.50 + 0.50 * warStat.getAvgWins()) * warStat.getAvgCards()));
+            warStat.setTotalGamesMissed(warStat.getGamesNotPlayed() + warStat.getCollectionGamesMissed());
+            playerAggregationWarStatsRepository.save(warStat);
+        }
+
+    }
+}
---
+++ src/main/resources/db/migration/V3.6__add_column_totalgamesmissed.sql
@@ -0,0 +1,1 @@
+ALTER TABLE player_aggregation_war_stats ADD total_games_missed int default 0;
