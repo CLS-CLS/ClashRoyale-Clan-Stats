4f4af6bbb8af8c52419b485332de2c3379126cb4
Christos <chrislytsikas@gmail.com>
2020-10-15 11:59:13 +0300

    riverrace: changed logging comments

--- src/main/java/org/lytsiware/clash/war2/scheduler/RiverRaceUpdateScheduler.java
+++ src/main/java/org/lytsiware/clash/war2/scheduler/RiverRaceUpdateScheduler.java
@@ -34,11 +34,11 @@ public class RiverRaceUpdateScheduler implements RunAtStartupJob {
         RiverRace riverRace = riverRaceRepository.activeRace().orElse(null);
         Job latestJob = jobRepository.findById(JOB_ID).orElse(null);
         if (riverRace == null) {
-            log.info("River race does not exist - should run");
+            log.info("River race does not exist");
             shouldRun = true;
         } else if (riverRace.getClan().getFinishTime() == null
                 && latestJob.getLatestExecution().plusHours(2).isBefore(LocalDateTime.now())) {
-            log.info("River race does finished and job has to run for at least 2 hours - should run");
+            log.info("River race has not finished and job has to run for at least 2 hours");
             shouldRun = true;
         } else {
 
@@ -47,10 +47,11 @@ public class RiverRaceUpdateScheduler implements RunAtStartupJob {
                 shouldRun = true;
             } else if (riverRace.getClan().getFinishTime() != null &&
                     latestJob.getLatestExecution().plusHours(23).isBefore(LocalDateTime.now())) {
-                log.info("Job has not run for at least 23 hours - should run");
+                log.info("Job has not run for at least 23 hours");
                 shouldRun = true;
             }
         }
+        log.info("Should run = {}", shouldRun);
         return shouldRun;
     }
 
@@ -60,8 +61,6 @@ public class RiverRaceUpdateScheduler implements RunAtStartupJob {
         log.info("River race scheduler triggered .. check if should run");
         if (shouldRun()) {
             doRun();
-        } else {
-            log.info("Scheduler has run recently and currentRace is finished - not need to run");
         }
     }
 
