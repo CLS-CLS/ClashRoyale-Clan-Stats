9410104b188a3a495e0a632c69bd8750c148fed6
Christos <chrislytsikas@gmail.com>
2020-11-05 23:17:05 +0200

    add better logging on riverrace scheduler execution check

--- src/main/java/org/lytsiware/clash/war2/scheduler/RiverRaceUpdateScheduler.java
+++ src/main/java/org/lytsiware/clash/war2/scheduler/RiverRaceUpdateScheduler.java
@@ -1,5 +1,6 @@
 package org.lytsiware.clash.war2.scheduler;
 
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.lytsiware.clash.core.domain.job.Job;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 
 @Service
@@ -27,26 +29,54 @@ public class RiverRaceUpdateScheduler implements RunAtStartupJob {
     private final RiverRaceManager riverRaceManager;
     private final RiverRaceRepository riverRaceRepository;
 
+    /**
+     * logs the appropriate message depenging on the boolean
+     */
+    @Builder
+    @Slf4j
+    private static class Description {
+        final String trueMessage;
+        final String falseMessage;
+
+        public Description(String trueMessage, String falseMessage) {
+            this.trueMessage = Optional.ofNullable(trueMessage).orElse("");
+            this.falseMessage = Optional.ofNullable(falseMessage).orElse("");
+        }
+
+        public boolean of(boolean bool) {
+            if (bool) {
+                log.info(trueMessage);
+            } else {
+                log.info(falseMessage);
+            }
+            return bool;
+        }
+
+    }
+
+    private static final Description description1 = new Description("River Race does not exist (should run)", "River race exists");
+    private static final Description description2 = new Description("River race has not finished", "River race finished");
+    private static final Description description3 = new Description("job has to run for at least 2 hours (should run)", "job has already run in less than 2 hours");
+    private static final Description description4 = new Description("job runs for first time (should run)", "There is a previous job that had run");
+    private static final Description description6 = new Description("Job has not run for at least 23 hours", "Previous job had run in less than 23 hours");
+
 
     @Override
     public boolean shouldRun() {
         boolean shouldRun = false;
         RiverRace riverRace = riverRaceRepository.activeRace().orElse(null);
         Job latestJob = jobRepository.findById(JOB_ID).orElse(null);
-        if (riverRace == null) {
-            log.info("River race does not exist");
+
+        if (description1.of(riverRace == null)) {
             shouldRun = true;
-        } else if (riverRace.getClan().getFinishTime() == null
-                && latestJob.getLatestExecution().plusHours(2).isBefore(LocalDateTime.now())) {
-            log.info("River race has not finished and job has to run for at least 2 hours");
+        } else if (description2.of(riverRace.getClan().getFinishTime() == null)
+                && description3.of(latestJob.getLatestExecution().plusHours(2).isBefore(LocalDateTime.now()))) {
             shouldRun = true;
         } else {
-
-            if (latestJob == null) {
-                log.info("job runs for first time - should run");
+            if (description4.of(latestJob == null)) {
                 shouldRun = true;
-            } else if (riverRace.getClan().getFinishTime() != null &&
-                    latestJob.getLatestExecution().plusHours(23).isBefore(LocalDateTime.now())) {
+            } else if (riverRace.getClan().getFinishTime() != null && description6.of(
+                    latestJob.getLatestExecution().plusHours(23).isBefore(LocalDateTime.now()))) {
                 log.info("Job has not run for at least 23 hours");
                 shouldRun = true;
             }
